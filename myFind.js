e424fa1836d94b1afa79408cfefbab8cd66f6166
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2023-01-19 08:04:03 -0800

    own developed find array method

---
+++ myFind.js
@@ -0,0 +1,16 @@
+function myFind(arr, fun) {
+  for (let i = 0; i < arr.length; i++) {
+    if (fun(arr[i])) {
+      //   return arr[i];
+      return i;
+    }
+  }
+}
+
+const number = [1, 4, 6, 24, 98];
+
+const find = myFind(number, function (value) {
+  return value === 6;
+});
+
+console.log(find);
