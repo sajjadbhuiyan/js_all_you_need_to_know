8490a5a5ced1a05167f576caf42ca3ba8081ae59
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2023-01-24 21:09:39 -0800

    myOOP2 practice running and hands on practice of inheritence

--- myOOP2.js
+++ myOOP2.js
@@ -1,13 +1,39 @@
-function Course() {}
+function extend(parant, child) {
+  child.prototype = Object.create(parant.prototype);
+  child.prototype.constructor = child;
+}
 
-Course.prototype = {
-  cost: function () {
-    console.log("Const is heigh");
-  },
-};
+function Routine(startTime, endTime) {
+  this.startTime = startTime;
+  this.endTime = endTime;
+}
 
-function College() {}
+// Routine.prototype.routine = function () {
+//   console.log(
+//     `Class time start from ${this.startTime} and end will be ${this.endTime}`
+//   );
+// };
 
-College.prototype.name = "NDC";
+function Semester(semesterName, duration, startTime, endTime) {
+  this.semesterName = semesterName;
+  this.duration = duration;
+  Routine.call(this, startTime, endTime);
 
-College.prototype = Object.create(Course.prototype);
+  this.semesterDescribe = function () {
+    console.log(
+      `This is ${this.semesterName} semester, duration is ${this.duration} months
+      Class start from ${startTime} and end ${endTime}`
+    );
+  };
+}
+
+extend(Routine, Semester);
+
+var semester = new Semester("7th", 6, "08:30", "02:30");
+
+function StudyYear(year) {
+  this.year = year;
+}
+
+StudyYear.prototype = Object.create(Routine.prototype);
+StudyYear.prototype = StudyYear;
