8f49e851a8623f1079af509a0a7758ba3d7a97ae
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2023-01-26 16:28:20 +0600

    this in Js class understanding

--- class.js
+++ class.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* class Rectangale {
   constructor(height, width) {
     this.height = height;
@@ -21,15 +23,35 @@ class Person {
     console.log(`Name is ${this.name} and Email is ${this.email}`);
   }
 
+  test() {
+    console.log(this);
+  }
+
   static create(str) {
     const person = JSON.parse(str);
     return new Person(person.name, person.email);
   }
 }
 
-const str =
-  '{"name": "Sajjad Bhuiya Taki", "email": "sajjadbhuiyan2018@gmail.com"}';
+// const str =
+//   '{"name": "Sajjad Bhuiya Taki", "email": "sajjadbhuiyan2018@gmail.com"}';
 
-const person = Person.create(str);
+// const person = Person.create(str);
 
-console.log(person);
+// console.log(person);
+
+const ss = new Person();
+
+const bb = ss.test;
+bb();
+
+function Rectangale() {
+  this.draw = function () {
+    console.log(this);
+  };
+}
+
+const rec = new Rectangale();
+
+const dra = rec.draw;
+dra();
